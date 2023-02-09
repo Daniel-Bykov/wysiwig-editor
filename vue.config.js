07af26ebf2b3b605087b8e3a65462f1ada6ae374
bykov-d <bykov-d@kcentr.ru>
2023-02-09 12:10:11 +0400

    #main: final build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/wysiwig-editor/" : "/",
+  publicPath: "",
   transpileDependencies: true,
 
   css: {
